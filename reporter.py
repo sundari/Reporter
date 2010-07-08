acb49a0a888594c6c6326de7da44803830e495aa
Raja S <rajajs@gmail.com>
2010-07-08 19:33:18 +0530

    Modified database to use a unique string as key instead of integer

--- reporter.py
+++ reporter.py
@@ -22,48 +22,48 @@ ID_QUIT = wx.NewId()
 
 
 class ReportDatabase():
-    def __init__(self, db_path):
-        """db_path is full path to the database"""
+    """The database holding the records.
+    This is a dictionary with keys made of a unique hash
+    and vals being a dict that has all the values for
+    each variable for that record.
+    eg:
+    {
+    'uniq_hash1':{'Name':'Raja', 'Age':38},
+    'uniq_hash2':{'Name':'Rajesh', 'Age':38}
+    }
+    """
+    def __init__(self, db_path, index_keys):
+        """db_path is full path to the database.
+        index_keys is a list of the keys that will
+        be used to make the index view"""
         self.db_path = db_path
-        self.db = shelve.open(db_path)        
+        # Note that open will create a new db if it does not exist
+        self.db = shelve.open(db_path)
+        self.index_keys = index_keys
 
-    def get_index(self, index_fields):
+    def get_index(self):
         """Get data from db to create an index.
         index_fields are the fields for creating the index
         return list of tuples"""
-        index_field_vals = [range(len(self.db))] # first value will be the id
-        # getting pretty hackish
-        for field in index_fields: 
-            index_field_vals.append([self.db[str(x)][field]
-                                     for x in range(self.get_max_id())
-                                     if str(x) in self.db.keys()])
-            
+        index_field_vals = []
+        
+        for field in self.index_keys:
+            index_field_vals.append([self.db[rec][field] for rec in self.db])
+
         vals = zip(*index_field_vals)
 
-        record_summary = {}
-        for i in range(len(vals)):
-            record_summary[i] = vals[i]
-
-        return record_summary
-
-
-    def get_max_id(self):
-        """Get the maximum id"""
-        return max([int(x) for x in self.db.keys()])
-
+        return vals
     
-    def insert_record(self, record_dict):
+    def insert_record(self, record):
         """Insert a record into the database.
-        record_dict is a dict
+        The key for each record is a unique id
+        constructed by joining all the index elements.
+        record is a dict
         eg: {'Name': 'Raja', 'Age': 39}"""
-        # find last id
-        try:
-            last_id = max([int(x) for x in self.db.keys()])
-        except ValueError: # empty dict
-            last_id = -1
-
+        unique_id = ''.join([str(record[k]) for k in self.index_keys])
+        
         # insert this record
-        self.db[str(last_id+1)] = record_dict
+        self.db[unique_id] = record
 
 
     def delete_record(self, rec_id):
@@ -75,7 +75,22 @@ class ReportDatabase():
         for rec in self.db: 
             print rec
 
+    def to_csv(self, fields, filename):
+        """export the database to a csv file.
+        fields is a tuple of header corresponding
+        to the dictionary keys"""
+        # string format for each row
+        row_format = '%s,' * len(fields) + '\n'
 
+        print 'fields', fields
+        with open(filename, 'w') as f:
+            # header row
+            f.write(row_format % fields)
+            # record per row
+            for record in self.db:
+                f.write(row_format % tuple([self.db[record][key]
+                                            for key in fields]))
+            
             
 class AutoWidthListCtrl(wx.ListCtrl, ListCtrlAutoWidthMixin, ColumnSorterMixin):
     def __init__(self, parent):
@@ -100,11 +115,10 @@ class Reporter(wx.Frame):
         
         # listcontrol
         self.record_display = AutoWidthListCtrl(panel)
-        self.record_display.InsertColumn(0, 'No.', width=30)
-        self.record_display.InsertColumn(1, 'Name', width=120)
-        self.record_display.InsertColumn(2, 'Age', width=30)
-        self.record_display.InsertColumn(3, 'Sex', width=60)
-        self.record_display.InsertColumn(4, 'Procedure Date', 100)
+        self.record_display.InsertColumn(0, 'Name', width=120)
+        self.record_display.InsertColumn(1, 'Age', width=30)
+        self.record_display.InsertColumn(2, 'Sex', width=60)
+        self.record_display.InsertColumn(3, 'Procedure Date', 100)
 
         # buttons
         #self.view_button = wx.Button(panel, -1, 'View Record')
@@ -122,17 +136,19 @@ class Reporter(wx.Frame):
         #self.hbox2.Add(self.update_button, 1, wx.ALL, 10)
         self.hbox2.Add(self.report_button, 1, wx.ALL, 5)
         
-        self.vbox.Add(self.hbox1, 5, wx.EXPAND, 5)
-        self.vbox.Add(self.hbox2, 1, wx.ALL, 5)
+        self.vbox.Add(self.hbox1, 6, wx.EXPAND, 10)
+        self.vbox.Add(self.hbox2, 1, wx.ALL, 10)
 
         self._build_menubar()
         
         self._set_bindings()
+
         # instantiate the db
-        self.db = ReportDatabase('/data/tmp/testdb')
-        self.rec_summary = self.db.get_index(
-            ('Demographics_Name', 'Demographics_Age',
-             'Demographics_Sex', 'Demographics_Date of Procedure'))
+        self.index_keys = ['Demographics_Name', 'Demographics_Age',
+                           'Demographics_Sex', 'Demographics_Date of Procedure']
+        self.db = ReportDatabase('/data/tmp/testdb', self.index_keys)
+        
+        self.rec_summary = self.db.get_index()
 
         self.show_records(self.rec_summary)
         self.record = {} #active record in memory
@@ -176,13 +192,17 @@ class Reporter(wx.Frame):
     def load_and_edit_record(self, event):
         """Load the selected record into a form for editing"""
         selected_record = self.record_display.GetFirstSelected()
+
         if selected_record == -1: # none selected
             return
 
-        self.edit_id = str(self.record_display.GetItem(selected_record, 0).GetText())
+        # convert to string coz unicode object does not work
+        selected_record_key = str(''.join([self.record_display.GetItem(
+                    selected_record, x).GetText()
+                    for x in range(len(self.index_keys))]))
 
-        rec = self.db.db[self.edit_id]
-        f = Form(self, 'report_docs/form_fields.yaml', self.edit_id)
+        rec = self.db.db[selected_record_key]
+        f = Form(self, 'report_docs/form_fields.yaml', selected_record_key)
         f.set_values(rec)
 
 
@@ -191,9 +211,10 @@ class Reporter(wx.Frame):
         selected_record = self.record_display.GetFirstSelected()
         if selected_record == -1: # none selected
             return
-        
-        self.db.delete_record(str(selected_record))
-        
+
+        db_key = self.rec_summary[selected_record][-1]
+        #self.db.delete_record()
+        #self.record_display.DeleteItem(2)
 
     def render_report(self, event):
         """Render selected record as a pdf"""
@@ -226,7 +247,10 @@ class Reporter(wx.Frame):
         
     def on_quit(self, event):
         """Close the db properly"""
+        #self.db.to_csv(self.index_keys, '/data/tmp/dump.csv')
+
         self.db.db.close()
+        
         sys.exit(0)
         
     def show_records(self, records):
@@ -235,19 +259,18 @@ class Reporter(wx.Frame):
         self.record_display.itemDataMap = records
 
         #self.record_display.ClearAll()
-        for ind in range(len(records)):
-            rec = records[ind]
-            self.record_display_append(rec, ind)
+        for rec in records:
+            self.record_display_append(rec)
 
 
-    def record_display_append(self, rec, ind):
+    def record_display_append(self, rec):
         """add the rec to display"""
-        index = self.record_display.InsertStringItem(sys.maxint, str(rec[0]))
-        self.record_display.SetStringItem(index, 1, rec[1])
-        self.record_display.SetStringItem(index, 2, str(rec[2]))
+        index = self.record_display.InsertStringItem(sys.maxint, rec[0])
+        self.record_display.SetStringItem(index, 1, str(rec[1]))
+        self.record_display.SetStringItem(index, 2, rec[2])
         self.record_display.SetStringItem(index, 3, rec[3])
-        self.record_display.SetStringItem(index, 4, rec[4])
-        self.record_display.SetItemData(index, ind)
+        #self.record_display.SetStringItem(index, 4, rec[4])
+        self.record_display.SetItemData(index, index)
             
             
     def new_record(self, event):
@@ -259,49 +282,19 @@ class Reporter(wx.Frame):
         Will be triggered from the form"""
         self.db.insert_record(self.record)
 
-        new_id = len(self.rec_summary)
-        #self.record_display.ClearAll()
-        #TODO: just append to rec_summary
-        rec_summary = [self.record[k] for k in ['Demographics_Name',
-                      'Demographics_Age', 'Demographics_Sex',
-                      'Demographics_Date of Procedure']]
-        rec_summary = [new_id] + rec_summary
-        self.record_display_append(rec_summary, new_id)
+        rec_summary = [self.record[k] for k in self.index_keys]
+        self.record_display_append(rec_summary)
         #self.show_records()
-        #self.db.close()
 
     def update_record(self, id):
         """Update the record with given id"""
         self.db.db[id] = self.record
         #TODO: update the record display
         
-def db_tests(path):
-    """Create a db in the path, and test it"""
-    print 'Creating db'
-    manager = ReportDatabase(path)
- 
-    print 'insert record'
-    reca = {'Name': 'Raja', 'Age': 38, 'Sex':'M'}
-    recb = {'Name': 'Rajan', 'Age': 39, 'Sex':'M'}
-    recc = {'Name': 'Rajee', 'Age': 48, 'Sex':'F'}
-
-    manager.insert_record(reca)
-    manager.insert_record(recb)
-    manager.insert_record(recc)
-
-    # debug
-    print manager.db.ids()
-    
-    print 'get index'
-    print manager.get_index(('Name', 'Age'))
-
-    print 'dump data'
-    print manager.dump()
-
 
 if __name__ == '__main__':
     app = wx.App()
-    rep = Reporter(None, -1, 'Records')
+    rep = Reporter(None, -1, 'EP Reports')
     app.MainLoop()
 
     
